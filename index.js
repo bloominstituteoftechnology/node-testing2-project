6c4f543d63d3d18e151dfd843fec44fdfe3407b2
Patrick Rodrigues <patrick5rodrigues@outlook.com>
2019-07-25 15:34:16 +0200

    set up server

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const server = require('./api/server.js');
+const server = require('./server.js');
 
 const port = process.env.PORT || 4000;
 server.listen(port, () => console.log(`\n**We're hearing from the ${port} **\n`));
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const cors = require("cors");
 const server = express();
 
-server.use(cors());
+const CPURouter = require("./pcparts/components/CPU/CPURouter");
+const GPURouter = require("./pcparts/components/GPU/GPURouter");
+
 server.use(express.json());
+
+server.use("/cpu", CPURouter)
+server.use("/gpu3", GPURouter)
 
 module.exports = server;
