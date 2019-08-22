722d3e0c16b5cc2f2328320d532ec162612f21a5
Yusuf Nafey <yusufnafey@yahoo.com>
2019-08-22 13:17:12 -0700

    implements get all superheroes endpoint

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const Superheroes = require("./superheroes-model");
 
 const server = express();
 
 server.use(express.json());
 
-server.get("/superheroes", (req, res) => {});
+server.get("/superheroes", (req, res) => {
+  Superheroes.get()
+    .then(superheroes => {
+      res.status(200).json(superheroes);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ message: "There was an error getting the superheroes." });
+    });
+});
 
 server.post("/add", (req, res) => {});
 
