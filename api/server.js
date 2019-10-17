bc92ad251977cdb0aa513a89645d5ffa9973b447
Daniel Wallen <daniel.wallen1987@gmail.com>
2019-10-17 15:26:25 -0400

    Index, server, knexfile, and package.json set-up.

---
+++ api/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+// Model goes here
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.status(200).json({ api: 'Up' });
+});
