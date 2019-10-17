95f76fac067f7dda03369c64a53402e9792a451d
Daniel Wallen <daniel.wallen1987@gmail.com>
2019-10-17 16:39:51 -0400

    MVP was met on previous commit (only asked for add/remove and those were already fine). Built endpoints and helper model for extra methods.

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 // Model goes here
+const Dogs = require('../dogs/dogsModel');
 
 const server = express();
 
@@ -8,6 +9,33 @@ server.use(express.json());
 
 server.get('/', (req, res) => {
     res.status(200).json({ api: "Up" });
+});
+
+server.get('/:id', (req, res) => {
+    const { id } = req.params;
+    Dogs.findById(id)
+        .then(dog => {
+            if (dog) res.status(200).json(dog)
+            else res.status(404).json({ message: 'No such dog' })
+        })
+        .catch(err => res.status(500).json({ message: err }))
+})
+
+server.post('/', (req, res) => {
+    const dogData = req.body;
+    Dogs.add(dogData)
+        .then(dog => res.status(201).json(dog))
+        .catch(err => res.status(500).json({ message: err }))
+});
+
+server.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    Dogs.remove(id)
+        .then(dog => {
+            if (dog) res.json({ removed: dog })
+            else res.status(404).json({ message: 'No such dog' })
+        })
+        .catch(err => res.status(500).json({ message: err }))
 });
 
 module.exports = server;
--- dogs/dogsModel.js
+++ dogs/dogsModel.js
@@ -2,14 +2,19 @@ const db = require('../data/dbConfig');
 
 module.exports = {
     insert,
-    remove
+    remove,
+    findById
 }
 
 async function insert(dog) {
     const [id] = await db('dogs').insert(dog, 'id');
     return db('dogs').where({id}).first(); // allows to check for specific dog
-}
+};
 
 function remove(id) {
     return db('dogs').where('id', Number(id)).delete();
-}
+};
+
+function findById(id) {
+    return db('dogs').where({ id }).first()
+};
