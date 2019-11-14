f7a814d2e06744126d68b87bdfb81b9c4bb431ca
Kevin Allen <kevin.allen91@gmail.com>
2019-11-14 13:08:47 -0800

    server running, tests functional

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 const express = require("express");
 const items = require("./itemsRouter");
 const server = express();
+const items = require("./itemsModel");
 
 server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.status(200).json({ message: `welcome to the ITEM` });
+});
+
+server.get("/items", (req, res) => {
+  items.find().then(item => res.status(200).json({ item }));
+});
+
+server.post("/items", (req, res) => {
+  items.add(item).then(item => res.status(201).json(item));
+});
