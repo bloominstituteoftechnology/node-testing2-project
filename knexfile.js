ba637a3dd49216460f88ddeb7445fbf79c1024c4
Ale <alearanibar@gmail.com>
2019-12-19 14:40:07 -0800

    knex file

--- knexfile.js
+++ knexfile.js
@@ -5,40 +5,67 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './dataBase/employee-record.db3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './dataBase/migrations'
+    },
+      seeds: {
+      directory: './dataBase/seeds'
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done)
+      }
     }
   },
 
-  staging: {
-    client: 'postgresql',
+  testing: {
+    client: 'sqlite3',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      filename: './dataBase/test.db3'
     },
+    useNullAsDefault: true,
     pool: {
-      min: 2,
-      max: 10
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done)
+      },
     },
     migrations: {
-      tableName: 'knex_migrations'
+      directory: './dataBase/migrations',
+    },
+    seeds: {
+      directory: "./dataBase/seeds"
     }
   },
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    client: "pg", // install this npm package
+    connection: process.env.DATABASE_URL,
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
+  },
 };
+
+
+
