294a08833d58faa7685df9a6047541a55082b70e
Kristin Parrish <kristinparrish@Kristins-MacBook-Pro.local>
2020-02-27 15:23:45 -0700

    added test server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-require("dontenv").config();
-const server = require(".api/server.js");
+require("dotenv").config();
+const server = require("./api/server.js");
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 
 server.listen(port, () =>{
     console.log(`\n** server listening on port: ${port} **\n`)
