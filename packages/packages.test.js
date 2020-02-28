f9205d79de865351d336ca20c2e396dd8769160b
agohorel <3317203+agohorel@users.noreply.github.com>
2020-02-27 23:03:40 -0500

    add tests for POST route

--- packages/packages.test.js
+++ packages/packages.test.js
@@ -2,8 +2,46 @@ require("dotenv").config();
 
 const request = require("supertest");
 const server = require("../server/server.js");
+const db = require("../db/connections.js");
 
-describe("packages router", () => {
+const seed = {
+  name: "aleph",
+  version: "0.4.1",
+  author: "alex gohorel",
+  license: "GPL v3.0"
+};
+
+const payload = {
+  name: "bend",
+  version: "0.0.1",
+  author: "alex gohorel",
+  license: "GPL v3.0"
+};
+
+beforeEach(async () => {
+  await db("packages").truncate();
+  await db("packages").insert(seed);
+});
+
+describe("packages router - POST", () => {
+  it("should return 201 on POST /api/packages", async () => {
+    const res = await request(server)
+      .post("/api/packages")
+      .send(payload);
+
+    expect(res.status).toBe(201);
+  });
+
+  it("should return the newly created resource in json format", async () => {
+    const res = await request(server)
+      .post("/api/packages")
+      .send(payload);
+
+    expect(res.body).toEqual(expect.objectContaining(payload));
+  });
+});
+
+describe("packages router - GET", () => {
   it("should return 200 OK on GET /api/packages", async () => {
     const res = await request(server).get("/api/packages");
     expect(res.status).toBe(200);
