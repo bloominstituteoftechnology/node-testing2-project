0a1b0d3abbe1e2295e77566bcf49c565d2d34235
agohorel <3317203+agohorel@users.noreply.github.com>
2020-02-27 22:14:12 -0500

    index prints environment along w/ port

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ const server = require("./server/server.js");
 const port = process.env.PORT || 5000;
 
 server.listen(port, () =>
-  console.log(`\n server listening at port ${port} \n`)
+  console.log(
+    `\n server listening at port ${port} using environment: ${process.env.DB_ENV} \n`
+  )
 );
