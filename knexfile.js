4940172a8481f5b3f1440be30678e6095596b7b9
thomas <t_huynh927@yahoo.com>
2020-06-10 20:20:59 -0700

    modded knexfile

--- knexfile.js
+++ knexfile.js
@@ -35,7 +35,10 @@ module.exports = {
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
     }
   }
 
