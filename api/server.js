359eeeab6877d48db7c0cca8f428d743c29f708c
s-munro <68410363+s-munro@users.noreply.github.com>
2021-01-28 18:27:36 -0500

    MVP

--- api/server.js
+++ api/server.js
@@ -1,7 +1,29 @@
 const express = require("express");
+const Meme = require("./memes/memes-model");
+const db = require("../data/dbConfig");
 
 const server = express();
 
 server.use(express.json());
+
+server.get("/memes", (req, res) => {
+  res.status(200).json("yeeha");
+});
+
+server.post("/memes", async (req, res) => {
+  Meme.insert(req.body)
+    .then((meme) => {
+      res.status(201).json(meme);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+
+server.delete("/memes/:id", async (req, res) => {
+  const { id } = req.params;
+  await Meme.remove(id);
+  res.status(200).json("woohoo!");
+});
 
 module.exports = server;
