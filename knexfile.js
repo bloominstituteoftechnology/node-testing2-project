16c8ddca4e85cae488822a606ebce62e45a171fa
jeanluciano <jeanluciano@mailbox.org>
2021-02-25 15:16:52 -0600

    mvp

---
+++ knexfile.js
@@ -0,0 +1,23 @@
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    migrations: { directory: './data/migrations' },
+    seeds: { directory: './data/seeds' },
+    connection: {
+      filename: './data/jedi.db3',
+    },
+  },
+  testing: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    migrations: { directory: './data/migrations' },
+    seeds: { directory: './data/seeds' },
+    connection: {
+      filename: './data/test.db3',
+    },
+  },
+  production: {
+
+  },
+};
