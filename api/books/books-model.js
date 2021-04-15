5401640133de8119c154d74fa8a3ed3b3947afe9
ngwatso <ngwatson79@yahoo.com>
2021-04-15 19:19:16 -0400

    MVP

--- api/books/books-model.js
+++ api/books/books-model.js
@@ -1,5 +1,16 @@
 const db = require('../../data/dbConfig');
 
+// ?? Get list of books
+const getAll = async () => {
+	return await db('books');
+};
+
+// ?? Get book with specified ID
+const getById = (id) => {
+	const book = db.first('*').from('books').where({ id });
+	return book;
+};
+
 // ?? Add a book to the database
 async function insert(book) {
 	return await db('books').insert(book);
@@ -10,4 +21,4 @@ async function remove(id) {
 	return await db('books').del().where({ id });
 }
 
-module.exports = { insert, remove };
+module.exports = { getAll, getById, insert, remove };
--- api/server.spec.js
+++ api/server.spec.js
@@ -2,6 +2,57 @@ const db = require('../data/dbConfig');
 const server = require('./server');
 const request = require('supertest');
 
-test('placeholder', () => {
-	expect(1).toEqual(1);
+// ?? Server test batch
+describe('server', () => {
+	// ?? Check environment
+	test('we are in testing environment', () => {
+		expect(process.env.DB_ENV).toBe('testing');
+	});
+
+	// ?? POST test batch
+	describe('POST /books', () => {
+		// ?? set up res to be used in all tests
+		let res;
+		beforeEach(async () => {
+			res = await request(server).post('/');
+		});
+
+		test('returns status 200 on successful post', () => {
+			return request(server)
+				.post('/books')
+				.send({ title: 'A Memory of Light' })
+				.then((res) => {
+					expect(res.status).toBe(201);
+				});
+		});
+		test('database contains posted data', () => {
+			return request(server)
+				.post('/books')
+				.send({ title: 'A New Spring' })
+				.then((res) => {
+					expect(res.type).toBe(
+						'application/json'
+					);
+				});
+		});
+	});
+
+	// ?? DELETE test batch
+	describe('DELETE /books', () => {
+		beforeEach(async () => {
+			await db.seed.run();
+		});
+		test('returns status 200 upon successful deletion', async () => {
+			const res = await request(server).delete(
+				'/books/1'
+			);
+			expect(res.status).toBe(200);
+		});
+		test('returns status 400 with invalid id', async () => {
+			const res = await request(server).delete(
+				'/books/4'
+			);
+			expect(res.status).toBe(404);
+		});
+	});
 });
