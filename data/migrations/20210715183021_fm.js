48d926ade354d79535ae19cf775e84099c991259
kato hawkins <kristhahawkins@icloud.com>
2021-07-15 18:44:43 -0400

    configured knexfile

--- data/migrations/20210715183021_fm.js
+++ data/migrations/20210715183021_fm.js
@@ -1,11 +1,11 @@
 
 exports.up = function(knex) {
-  return knex.schema.createTable("krusty krew", tbl =>{
+  return knex.schema.createTable("krustykrew", tbl =>{
       tbl.increments()
       tbl.string("name", 255).unique().notNullable
   })
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTableIfExists("krusty krew")
+  return knex.schema.dropTableIfExists("krustykrew")
 };
--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,24 @@
-// Update with your config settings.
+const common = {
+  client: 'sqlite3',
+  useNullAsDefault: true,
+  migrations: { directory: './data/migrations' },
+  seeds: { directory: './data/seeds' },
+}
 
 module.exports = {
-
-  development: {
-    client: 'sqlite3',
+  development: { // process.env.DB_ENV || 'development'
+    ...common,
     connection: {
-      filename: './dev.sqlite3'
-    }
+      filename: './data/krustykrew.db3',
+    },
   },
-
-  staging: {
-    client: 'postgresql',
+  testing: { // process.env.DB_ENV 'testing'
+    ...common,
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      filename: './data/test.db3',
     },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
   },
-
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
 
+  },
 };
