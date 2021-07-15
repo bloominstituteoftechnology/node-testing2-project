f91209ff4d40f1f32ad9c9175cc79b8b149d68ed
kato hawkins <kristhahawkins@icloud.com>
2021-07-15 18:36:20 -0400

    updated and completed migration file

--- data/migrations/20210715183021_fm.js
+++ data/migrations/20210715183021_fm.js
@@ -1,8 +1,11 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("krusty krew", tbl =>{
+      tbl.increments()
+      tbl.string("name", 255).unique().notNullable
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists("krusty krew")
 };
