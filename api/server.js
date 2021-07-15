75fcc8cf69314bbc77e6f97fbb546f25e002bfbe
kato hawkins <kristhahawkins@icloud.com>
2021-07-15 19:18:35 -0400

    updated server.js

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const server = express()
+const KrustyKrew = require('./krustykrew/krustykrew-model.js')
 
 server.use(express.json())
 
@@ -7,5 +8,31 @@ server.get("/", (req,res) =>{
     res.status(200).json({api:"up!!"})
 })
 
+server.get("/krustykrew", (req,res) =>{
+    KrustyKrew.getAll()
+    .then(krustykrew => {
+        res.status(200).json(krustykrew)
+    })
+    .catch(e =>{
+        res.status(500).json(e)
+    })
+})
 
+server.get("/krustykrew/id", (req,res) =>{
+    res.end()
+})
+
+server.post("/krustykrew", async (req, res) => {
+    res
+      .status(201)
+      .json(await KrustyKrew.insert(req.body))
+  });
+  server.delete("/krustykrew/:id", (req, res) => {
+    res.end()
+  });
+
+  server.put("/krustykrew/:id", (req, res) => {
+    res.end()
+  });
+  
 module.exports = server
