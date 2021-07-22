84687b849c46e21b55404d84896e56f9528e476f
AdrianHDev <adrianhensondev@gmail.com>
2021-07-22 14:52:06 -0400

    project finish

--- api/pokemon-model.js
+++ api/pokemon-model.js
@@ -1,5 +1,9 @@
 const db = require('../data/db-config')
 
+const getAll = () => {
+    return db("pokemon");
+}
+
 const addmon = pokemon => {
     return db("pokemon").insert(pokemon)
 }
@@ -7,4 +11,4 @@ const addmon = pokemon => {
 const delmon = pokemon_id => {
     return db("pokemon").where({pokemon_id}).delete()
 }
-module.exports = { addmon, delmon }
+module.exports = { addmon, delmon, getAll }
--- api/pokemon-router.js
+++ api/pokemon-router.js
@@ -1,21 +1,31 @@
 const express = require("express");
 const router = express.Router();
-const Pokemon = require('./pokemon-model')
+const Pokemon = require("./pokemon-model");
 
-router.post('/', async (req, res, next) => {
-    let x = await Pokemon.addmon(req.body);
-    res.status(201).json(x)
-})
+router.get("/", async (req, res, next) => {
+  let x = await Pokemon.getAll();
+  res.json(x);
+});
 
-router.delete('/:pokemon_id', async (req, res, next) => {
-    try {
-        await Pokemon.delmon(req.params.pokemon_id)
-        res.status(204).json({message: "Deleted successfully"})
-    } catch (error) {
-        console.error(error);
-        res.status(500).json("Internal server error...");
+router.post("/", async (req, res, next) => {
+  let x = await Pokemon.addmon(req.body);
+  res.status(201).json(x);
+});
+
+router.delete("/:pokemon_id", async (req, res, next) => {
+  try {
+    let x = await Pokemon.delmon(req.params.pokemon_id);
+    if (x !== 0) {
+      res.status(200).json({
+        message: `Pokemon with ID: ${req.params.pokemon_id} deleted`,
+      });
+    } else {
+        res.status(404).json({message: `Pokemon with ID: ${req.params.pokemon_id} not found.`})
     }
-})
-
+  } catch (error) {
+    console.error(error);
+    res.status(500).json("Internal server error...");
+  }
+});
 
 module.exports = router;
