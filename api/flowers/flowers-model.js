a13b7d6ae5e9978ff9f52a857677b83702e0ed7c
Garrett Yien <yien.garrett94@gmail.com>
2021-09-16 19:52:53 -0400

    completed db and model and router

---
+++ api/flowers/flowers-model.js
@@ -0,0 +1,28 @@
+const db = require('../../data/db-config');
+
+async function get() {
+    return await db('flowers')
+}
+
+async function add(object) {
+    const [id] = await db('flowers').insert(object)
+    const flower = await db('flowers')
+        .where('id', id)
+        .first()
+    return flower
+}
+
+async function del(id) {
+    await db('flowers')
+    .delete()
+    .where('id', id)
+    return await db('flowers')
+}
+
+
+
+module.exports = {
+    get,
+    add,
+    del
+};
