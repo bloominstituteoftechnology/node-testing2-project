f0686c7d74984fcfc1b402409c23fd1b62841d9a
nhamilton1 <nickh3592@gmail.com>
2021-10-14 14:39:48 -0400

    more testing

--- api/cars/cars-model.test.js
+++ api/cars/cars-model.test.js
@@ -28,5 +28,49 @@ describe('Car.getAll()', () => {
         expect(cars).toHaveLength(4)
     })
 
-    
+    it('cars returned are the correct shape', async () => {
+        expect(cars).toMatchObject([
+            {
+                vin: '1FVACWCS96HV81220',
+                make: 'Ford',
+                model: 'Focus',
+                mileage: 175000,
+                title: 'fine',
+                transmission: 'manual'
+            },
+            {
+                vin: '3GCEC14X66G218202',
+                make: 'Honda',
+                model: 'Civic',
+                mileage: 1700,
+                title: 'fine',
+                transmission: 'automatic'
+            },
+            {
+                vin: 'JH4DC4440RS004255',
+                make: 'Honda',
+                model: 'CRV',
+                mileage: 4000,
+                title: 'great',
+                transmission: 'automatic'
+            },
+            {
+                vin: 'WVGBV7AX6CW559712',
+                make: 'Toyota',
+                model: 'Rav4',
+                mileage: 1,
+                title: 'great',
+                transmission: 'automatic'
+            },
+        ])
+    })
+})
+
+describe('car.getById(id)', () => {
+    test('should get correct car by its id', async () => {
+        const toyota = await Cars.getById(4)
+        expect(toyota).toMatchObject({ make: 'Toyota' })
+        const honda = await Cars.getById(3)
+        expect(honda).toMatchObject({ make: 'Honda' })
+    })
 })
