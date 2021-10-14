f5272bac150dd484476b8d2d92f079ab00cda66f
Klove Adams <kloveadams@gmail.com>
2021-10-14 19:20:31 -0400

    still working on 13, takeing a brake

--- api/games/games-router.js
+++ api/games/games-router.js
@@ -1,11 +1,18 @@
 const router = require("express").Router();
 const Game = require("./games-model");
 
-router.get("/", (req, res, next) => {
-  Game.getAll().then(games => {
-    res.json(games)
-  })
+router.get("/", (req, res) => {
+  Game.getAll().then(games => { res.json(games) })
 });
+
+router.post("/", async(req, res, next) => {
+  try {const newGame = await Game.insert(req.body)
+  res.status(201).json({ newGame })
+} catch {
+  next()
+}
+});
+
 
 // router.get("*", (req, res, next) => {
 //   res.json("router");
