b9957590e251146955dbfa4bf4a5a89bc0acac7d
Klove Adams <kloveadams@gmail.com>
2021-10-14 16:09:42 -0400

    server works

--- api/server.js
+++ api/server.js
@@ -2,6 +2,11 @@ const express = require("express");
 
 const server = express();
 
+server.get("*", (req, res, next) => {
+  res.json("server")
+  next()
+})
+
 server.use((err, req, res, next) => { // eslint-disable-line
   res.status(err.status || 500).json({ message: err.message });
 });
