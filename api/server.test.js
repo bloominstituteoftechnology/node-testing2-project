21e5cf4fa42543cd5270b86ec68f19c3effdcae5
Klove Adams <kloveadams@gmail.com>
2021-10-14 18:26:03 -0400

    starting work in server.test.js

---
+++ api/server.test.js
@@ -0,0 +1,16 @@
+const request = require("supertest");
+const server = require("./server");
+const db = require("../data/dbConfig");
+
+beforeAll(async () => {
+  await db.migrate.rollback();
+  await db.migrate.latest();
+});
+
+beforeEach(async () => {
+  await db.seed.run();
+});
+
+afterAll(async () => {
+  await db.destroy();
+});
