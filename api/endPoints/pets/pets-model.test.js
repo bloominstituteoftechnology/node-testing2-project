30cf430ac687164538ee8729ee50d5dd9cab7917
Rick <ricksmycodeguy@gmail.com>
2021-10-14 19:22:00 -0500

    All pets-modle.test.js passing

--- api/endPoints/pets/pets-model.test.js
+++ api/endPoints/pets/pets-model.test.js
@@ -26,7 +26,34 @@ describe('Pet db access functions', () => {
         it('returns the correct pet shape', async () => {
             const pets = await Pets.getAll();
             expect(pets[0]).toHaveProperty('name', 'Buddy');
-            // expect(pets[0]).toMatchObject({ pet_id: 1, name: 'Ella'});
+            expect(pets[1]).toMatchObject({ pet_id: 2, name: 'Ella' });
+        });
+    });
+
+
+    describe('Pets.insert', () => {
+        it('adds a new pet to the table', async () => {
+            const newPet = { pet_id: 5, name: 'Rover' };
+            await Pets.insert(newPet);
+            const pets = await db('pets');
+            expect(pets).toHaveLength(5);
+        });
+        it('returns the newly created pet', async () => {
+            const pet = { pet_id: 5, name: 'Rover' };
+            const newPet = await Pets.insert(pet);
+            expect(newPet).toMatchObject(pet);
+        });
+    });
+
+    describe('Pets.remove', () => {
+        it('removes a pet from the table', async () => {
+            await Pets.remove(1);
+            const currentPets = await db('pets');
+            expect(currentPets).toHaveLength(3);
+        });
+        it('returns the deleted pet', async () => {
+            const removed = await Pets.remove(1);
+            expect(removed).toMatchObject({ pet_id: 1, name: 'Buddy'});
         });
     });
 });
