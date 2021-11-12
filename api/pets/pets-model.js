fa313dcaa9164cbfc21c92d65dd38cd3f16ec961
Kat <kyevsukov@gmail.com>
2021-11-11 21:11:08 -0500

    server set up

--- api/pets/pets-model.js
+++ api/pets/pets-model.js
@@ -2,8 +2,6 @@ const db = require('../../data/dbConfig.js')
 
 module.exports = {
   insert,
-  update,
-  remove,
   getAll,
   getById,
 }
--- api/server.js
+++ api/server.js
@@ -1,5 +1,33 @@
 const express = require("express");
+const Pets = require("./pets/pets-model.js");
 
 const server = express();
 
 server.use(express.json());
+
+
+server.get("/", (req, res) => {
+    res.status(200).json({ api: "running" });
+  });
+
+  server.get("/pets", (req, res) => {
+    Pets.getAll()
+      .then(pets => {
+        res.status(200).json(pets);
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  });
+  
+  server.get("/pets/:id", async (req, res) => {
+    res.json(await Pets.getById(req.params.id))
+  });
+  
+  server.post("/pets", async (req, res) => {
+    res
+    .status(201)
+    .json(await Pets.insert(req.body))
+  });
+
+  module.exports = server;
