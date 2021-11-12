1745a21a803f6e1d7d7cba17f5d6f0f5b430553d
monicasalas86 <monicsalas167@gmail.com>
2021-11-12 00:12:48 -0600

    made skeleton code for model file

---
+++ api/starks/starks-model.js
@@ -0,0 +1,25 @@
+const db = require('../../data/db-config')
+
+module.exports = {
+  getAll,
+  getById,
+  insert,
+  update,
+  remove
+}
+
+function getAll() {
+
+}
+function getById() {
+
+}
+function insert() {
+
+}
+function update() {
+
+}
+function remove() {
+
+}
