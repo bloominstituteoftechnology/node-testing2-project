352e5d3535da3c5d8b3754f99b0e9324127aaaa9
monicasalas86 <monicsalas167@gmail.com>
2021-11-12 00:19:11 -0600

    made skelleton code for endpoints

--- api/server.js
+++ api/server.js
@@ -7,4 +7,11 @@ server.get('/', (req, res) => {
   res.status(200).json('hello from api')
 })
 
+server.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack
+  })
+})
+
 module.exports = server
