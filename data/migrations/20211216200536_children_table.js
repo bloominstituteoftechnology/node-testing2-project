6d5ac6c4ac1d74d0def085a476fe6d0e57c1eb6b
Dave Madden <davelearnsstuff@gmail.com>
2021-12-16 12:26:28 -0800

    db setup and seed complete

--- data/migrations/20211216200536_children_table.js
+++ data/migrations/20211216200536_children_table.js
@@ -2,6 +2,9 @@
 exports.up = async function(knex) {
   await knex.schema.createTable('children', table=>{
       table.increments('child_id')
+      table.text('first_name').notNullable()
+      table.text('last_name').notNullable()
+      table.text('hobbies')
   })
 };
 
