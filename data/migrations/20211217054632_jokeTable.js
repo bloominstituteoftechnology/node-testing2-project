c85e63f8c9cc75eed435429f6b9b1229fafe0d74
markporo <markporo@yahoo.com>
2021-12-17 02:27:02 -0500

    installed packages and setting up all files, migration and seed files set up

---
+++ data/migrations/20211217054632_jokeTable.js
@@ -0,0 +1,13 @@
+
+exports.up = function (knex) {
+    return knex.schema
+        .createTable("jokes", (jokes) => {
+            jokes.increments("id");
+            jokes.string("joke_question", 2048).notNullable().unique()
+            jokes.string("joke_answer", 2048).notNullable()
+        })
+};
+
+exports.down = function (knex) {
+
+};
---
+++ knexfile.js
@@ -0,0 +1,61 @@
+const sharedConfig = {
+  client: 'sqlite3',
+  useNullAsDefault: true,
+  migrations: {
+    directory: './data/migrations',
+  },
+  seeds: {
+    directory: './data/seeds',
+  },
+  pool: {
+    afterCreate: (conn, done) => {
+      conn.run('PRAGMA foreign_keys = ON', done)
+    },
+  },
+}
+
+module.exports = {
+  development: {
+    ...sharedConfig,
+    connection: { filename: './data/database.db3' },
+  },
+  testing: {
+    ...sharedConfig,
+    connection: { filename: './data/testing.db3' },
+  },
+}
+
+
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
+
+//   production: {
+//     client: 'postgresql',
+//     connection: {
+//       database: 'my_db',
+//       user:     'username',
+//       password: 'password'
+//     },
+//     pool: {
+//       min: 2,
+//       max: 10
+//     },
+//     migrations: {
+//       tableName: 'knex_migrations'
+//     }
+//   }
+
+// };
