562589a879983d73c4df4951532aea9798287ed6
RFNoC <rfnoc@rfnocs-pro-51.lan>
2022-01-27 19:33:37 -0800

    User model tests - scaffolding

--- api/users/users-model.test.js
+++ api/users/users-model.test.js
@@ -5,3 +5,29 @@ test('[1] NODE_ENV is correct', () => {
     expect(process.env.NODE_ENV).toBe('testing')
   })
 
+  beforeAll(async () => {
+    await db.migrate.rollback()
+    await db.migrate.latest()
+  })
+  beforeEach(async () => {
+    await db.seed.run()
+  })
+  afterAll(async () => {
+    await db.destroy()
+  })
+
+  describe('User model', () => {
+    describe('User.find()', () => { 
+      test.todo('[2] returns all users in table')
+      test.todo('[3] returned users have id and name')
+    })
+
+    describe('User.findById(id)', () => { 
+      test.todo('[4] returns the correct user')
+    })
+
+    describe('User.add(User)', () => { 
+      test.todo('[5] db updates with the new user')
+      test.todo('[6] resolves the newly created user')
+    })
+  })
