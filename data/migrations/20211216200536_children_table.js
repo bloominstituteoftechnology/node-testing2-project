a7c357b6ecc134c54ed436106dc108712954cf56
FiftyShadesofCode <philiprmcdavid@gmail.com>
2022-01-27 19:03:27 -0500

    sheesh! That was a doozy!

---
+++ data/migrations/20211216200536_children_table.js
@@ -0,0 +1,13 @@
+
+exports.up = async function(knex) {
+  await knex.schema.createTable('children', table=>{
+      table.increments('child_id')
+      table.text('first_name').notNullable()
+      table.text('last_name').notNullable()
+      table.text('hobbies')
+  })
+};
+
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists('children')
+};
