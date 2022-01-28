bfd6ec40f8db11fe1eef840722c5068800188654
SheebaHashmi <sheebahashmi0@gmail.com>
2022-01-27 22:36:31 -0500

    Create more test for register enpoint

--- api/server.test.js
+++ api/server.test.js
@@ -32,26 +32,73 @@ describe('GET /users',()=> {
     })
 })
 describe('Register users',()=>{
-    let res
-    beforeEach(async ()=> {
-         res = await request(server)
+   
+    test('return a status 201',async ()=> {
+        const res = await request(server)
         .post('/api/auth/register')
         .send({ username: "foo", password: "1234", role_name:"instructor"})
-    })
-    test('return a status 201',()=> {
         expect(res.status).toBe(201)
     })
-    test('return the new user', ()=> {
+    test('return the new user',async ()=> {
+        const res = await request(server)
+        .post('/api/auth/register')
+        .send({ username: "foo", password: "1234", role_name:"instructor"})
         expect(res.body).toMatchObject({user_id:3,username: "foo", role_name:"instructor"})
         expect(res.body).toHaveProperty('user_id','username','role_name')
     })
+    test('response with default role',async()=> {
+        const res = await request(server)
+        .post('/api/auth/register')
+        .send({ username: "bar", password: "1234"})
+
+        expect(res.body).toHaveProperty("role_name","student")
+    })
+    test('error reponse on invalid role',async()=> {
+        const res = await request(server)
+        .post('/api/auth/register')
+        .send({ 
+            username: "bar",
+            password: "1234",
+            role_name:"13u213io3u21oiu321ou312oi32u213iou312iouio132uoi312uoi2134yui5yui34yui42yui14yiu34ui41341uiy41u2i41uiy412uiy421uiy412iu"
+        })
+
+        expect(res.body.message).toBe("Role name can not be longer than 32 chars")
+    })
 })
 describe('Login Users',()=> {
-    test('return a status 200',async()=> {
-        const res = await request(server)
+    let res
+    beforeEach(async() => {
+        res = await request(server)
         .post('/api/auth/login')
         .send({  username: "sue", password: "1234"})
-
+    })
+    test('return a status 200',async()=> {
         expect(res.status).toBe(200)
     })
+    test('response of login user', () => {
+        expect(res.body).toHaveProperty("message")
+        expect(res.body).toHaveProperty("token")
+    })
+    
+})
+describe('GET /user/id',() => {
+    test('returns a status 401 and invalid token message',async()=> {
+        const res = await request(server)
+        .get(`/api/users/1`)
+        .set("Authorization","garbage token")
+        expect(res.status).toBe(401)
+        expect((res)=> {
+            res.body.message = "Token invalid"
+        })
+    })
+    test('returns a status 403 and restriction message',async()=> {
+        const res = await request(server)
+        .get(`/api/users/1`)
+        .set("Authorization","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWJqZWN0IjoyLCJ1c2VybmFtZSI6InN1ZSIsInJvbGVfbmFtZSI6Imluc3RydWN0b3IiLCJpYXQiOjE2NDMzNDAwMjMsImV4cCI6MTY0MzQyNjQyM30.zedD3fepHK06AgYokUDIBPnblKRtUJpogaK9GUVbzRg")
+        expect(res.status).toBe(403)
+        expect((res)=> {
+            res.body.message = "This is not for you"
+        })
+    })
+   
 })
