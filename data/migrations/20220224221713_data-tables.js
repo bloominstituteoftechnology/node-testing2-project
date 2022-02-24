e6aa04457c17412af793b32a320b635982a92f03
David Conley <dconley1989@gmail.com>
2022-02-24 16:23:07 -0700

    created migration and seed files

--- data/migrations/20220224221713_data-tables.js
+++ data/migrations/20220224221713_data-tables.js
@@ -4,15 +4,29 @@
  */
 exports.up = function (knex) {
   return knex.schema
-    .createTable("Quarterbacks")
-    .increments("quarterback_id")
-    .string("quarterback_name")
-    .createTable("Teams")
-    .increments("");
+    .createTable("Quarterbacks", (tbl) => {
+      tbl.increments("quarterback_id");
+      tbl.string("quarterback_name").notNullable();
+    })
+    .createTable("Teams", (tbl) => {
+      tbl.increments("team-id");
+      tbl.string("team_name").notNullable();
+      tbl
+        .integer("quarterback_id")
+        .notNullable()
+        .unsigned()
+        .references("quarterback_id")
+        .inTable("Quarterbacks")
+        .onDelete("Restrict");
+    });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function (knex) {};
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists("Quarterbacks")
+    .dropTableIfExists("Teams");
+};
