d80ff94bbb1e17ea04142e78605e77ce6a7f4095
Jessica Kitchen <jess@Jessicas-Mac-mini.local>
2022-02-25 02:05:10 -0500

    completed testing

--- api/server.js
+++ api/server.js
@@ -10,16 +10,28 @@ server.get('/', (req, res) => {
     res.status(200).json({ api: 'up'});
 })
 
-server.get('/api/snacks', (req, res, next) => {
-    res.json('snacks get linked');
+server.get('/snacks', (req, res, next) => {
+    Snack.getAll()
+    .then(snacks => {
+        res.status(200).json(snacks);
+    }) 
+    .catch(next)
 })
 
-server.get('/api/snacks/:id', (req, res, next) => {
-    res.json(' snack getById linked');
+server.get('/snacks/:id', (req, res, next) => {
+    Snack.getById(req.params.id)
+    .then(snack => {
+        res.status(200).json(snack);
+    })
+    .catch(next)
 })
 
-server.post('/api/snacks', (req, res, next) => {
-    res.json('snack post linked');
+server.post('/snacks', (req, res, next) => {
+    Snack.insert(req.body)
+    .then(snack => {
+        res.status(200).json(snack);
+    })
+    .catch(next)
 })
 
 server.use((err, req, res, next) => { // eslint-disable-line
--- api/server.test.js
+++ api/server.test.js
@@ -27,7 +27,57 @@ describe('snacks model', () => {
 describe('server endpoints', () => {
     it('call the `up` endpoint', async () => {
         const res = await request(server).get('/');
+        
         expect(res.status).toBe(200);
         expect(res.body).toEqual({ api: "up" });
     });
-})
+
+    it('[GET] /snacks', async () => {
+        let res = await request(server).get('/snacks');
+
+        expect(res.status).toBe(200);
+        expect(res.body).toBeInstanceOf(Array);
+        expect(res.body).toHaveLength(0);
+
+        await Snack.insert({snack_name: 'Navi Pretzel', park_id: 2 });
+        res = await request(server).get('/snacks');
+
+        expect(res.body).toHaveLength(1);
+    });
+
+    it('should return JSON', async () => {
+        const res = await request(server).get('/snacks');
+
+        expect(res.type).toBe('application/json')
+    });
+
+    it('should return a list of snacks', async () => {
+        await Snack.insert({ snack_name: 'Navi Pretzel' });
+        await Snack.insert({ snack_name: 'Kakigori' });
+        const res = await request(server).get('/snacks');
+
+        expect(res.body).toHaveLength(2);
+    });
+    it('[GET] /snacks/:id', async () => {
+        let res = await Snack.insert({ snack_name: 'Navi Pretzel' });
+        res = await request(server).get(`/snacks/1`);
+
+        expect(res.status).toBe(200);
+        expect(res.body.snack_name).toBe('Navi Pretzel');
+    });
+    it('[POST] /snacks', async () => {
+        let res = await request(server)
+        .post('/snacks')
+        .send({ snack_name: 'BBQ Mac n Cheese' });
+        
+        expect(res.status).toBe(200);
+    });
+    it('responds with the newly created snack', async () => {
+        const res = await request(server).post('/snacks').send({snack_name: 'Street Corn'})
+        expect(res.body).toMatchObject({snack_name: 'Street Corn'})
+    })
+    it('responds with a 422 if no name in payload', async () => {
+        const res = await request(server).post('/snacks').send({})
+        expect(res.status).toBe(500)
+    })
+});
