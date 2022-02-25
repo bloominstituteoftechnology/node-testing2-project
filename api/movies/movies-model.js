f8bf7d8373e5b0c146f0ca64c04811237e08850e
Mingjun Geng <sooofeve@gmail.com>
2022-02-24 22:34:06 -0800

    test

---
+++ api/movies/movies-model.js
@@ -0,0 +1,34 @@
+
+const db = require('../../data/db-config')
+
+module.exports = {
+  insert,
+  update,
+  remove,
+  getAll,
+  getById,
+}
+
+function getAll() {
+  return db('movies')
+}
+
+function getById(id) {
+    return db('movies')
+      .where({ id })
+      .first()
+  }
+  
+  async function insert(movie) {
+    const [id] = await db('movies').insert(movie)
+    return getById(id)
+  }
+  
+
+async function update(id, changes) {
+  return null
+}
+
+function remove(id) {
+  return null
+}
