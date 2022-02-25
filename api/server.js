01565487fbb4cd1a259213ebc5ed6e9680a68aaf
Jessica Kitchen <jess@Jessicas-Mac-mini.local>
2022-02-25 00:28:32 -0500

    completed express app build

--- api/server.js
+++ api/server.js
@@ -1,30 +1,32 @@
 const express = require('express');
 
-// const Snack = require('./snacks/snacks-model');
+const Snack = require('./snacks/snacks-model');
 
 const server = express();
 
 server.use(express.json());
 
-// server.get('/', (req, res) => {
-//     res.status(200).json({ api: 'up'});
-// })
+server.get('/', (req, res) => {
+    res.status(200).json({ api: 'up'});
+})
 
-// server.get('/snacks', (req, res, next) => {
-//     res.json('snacks get linked');
-// })
+server.get('/snacks', (req, res, next) => {
+    res.json('snacks get linked');
+})
 
-// server.get('/snacks/:id', (req, res, next) => {
-//     res.json(' snack getById linked');
-// })
+server.get('/snacks/:id', (req, res, next) => {
+    res.json(' snack getById linked');
+})
 
-// server.post('/snacks', (req, res, next) => {
-//     res.json('snack post linked');
-// })
+server.post('/snacks', (req, res, next) => {
+    res.json('snack post linked');
+})
 
-// server.use((err, req, res, next) => { // eslint-disable-line
-//     res.status(err.status || 500).json({
-//       message: err.message,
-//       stack: err.stack,
-//     })
-//   })
+server.use((err, req, res, next) => { // eslint-disable-line
+    res.status(err.status || 500).json({
+      message: err.message,
+      stack: err.stack,
+    })
+  })
+
+  module.exports = server;
--- index.js
+++ index.js
@@ -3,4 +3,4 @@ require('dotenv').config();
 const server = require('./api/server.js');
 
 const port = process.env.PORT || 9000;
-server.listen(port, () => console.log(`\n** server up on port ${port} **\n`));
+server.listen(port, () => console.log(`magic happening on port ${port}`)); 
