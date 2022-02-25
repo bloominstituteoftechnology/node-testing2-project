68378c84a9c2458730db2268ffd69930104a27b1
Mingjun Geng <sooofeve@gmail.com>
2022-02-24 22:09:30 -0800

    database work

---
+++ data/migrations/20220225055216_01-movies-table.js
@@ -0,0 +1,11 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('movies', tbl =>{
+        tbl.increments()
+        tbl.string("name", 255).unique().notNullable();
+    }) 
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('movies')
+};
