937a9d5ce3c0e094ba37558109ca2c4f806568a0
Andrew Gary <andrew.gary91@gmail.com>
2022-04-01 01:38:31 -0500

    WIP

---
+++ knexfile.js
@@ -0,0 +1,17 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './dev.sqlite3'
+    }
+  },
+
+  useNullAsDefault: true,
+
+};
