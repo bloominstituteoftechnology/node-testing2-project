3ac36011d92978a2f46098f911a11bafd64af822
Ethan Miles <ethanmilescs@gmail.com>
2022-04-28 17:03:33 -0500

    added routes

--- api/server.js
+++ api/server.js
@@ -11,4 +11,20 @@ server.get("/", (req,res) => {
     res.status(200).json({ api: "up and running señor"})
 })
 
+server.get('/crossfitters', (req,res) => {
+    res.json('you found me')
+})
+
+server.get('/crossfitters/:id', (req,res) => {
+    res.json('you found me again!')
+})
+
+server.post('/crossfitters', (req,res) => {
+    res.json('you are getting good at this')
+})
+
+server.delete('/crossfitters/:id', (req,res) => {
+    res.json('you mastered it')
+})
+
 module.exports = server
