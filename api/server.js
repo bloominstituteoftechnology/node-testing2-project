c7e63a7e059aab243571e2ac1883b5d93034adfa
Andre Blaze <kuraiokamiblaze@gmail.com>
2022-04-28 18:10:08 -0600

    Model Tests_6 Tests done

--- api/server.js
+++ api/server.js
@@ -7,8 +7,17 @@ const server = express();
 server.use(express.json());
 
 server.get('/', (req, res) => {
-    res.status(200).json({ api: 'up' })
+    res.status(200).json({ api: 'up' });
 })
 
+server.get('/smash', (req, res) => {
+    Smash.getAll()
+        .then(characters => {
+            res.status(200).json(characters);
+        })
+        .catch(err => {
+            res.status(404).json('characters not found')
+        })
+})
 
 module.exports = server
--- api/server.test.js
+++ api/server.test.js
@@ -32,3 +32,48 @@ describe('Server Tests', () => {
         expect(res.body).toEqual({ api: 'up' })
     })
 })
+
+describe('Model Tests', () => {
+    test('[3] getAll', async() => {
+        const result = await Smash.getAll();
+        expect(result.length).toBe(3);
+        expect(result[0]).toMatchObject({ id: 1, name: 'Kirby', series: 'Kirby' });
+        expect(result[1]).toMatchObject({ id: 2, name: 'Incineroar', series: 'Pokemon' });
+        expect(result[2]).toMatchObject({ id: 3, name: 'Sora', series: 'Kingdom Hearts' });
+
+    });
+    test('[4] getById', async () => {
+        let result =  await Smash.getById(2);
+        expect(result).toBeDefined();
+        expect(result.name).toBe('Incineroar');
+        expect(result.series).toBe('Pokemon');
+
+        result = await Smash.getById(500);
+        expect(result).not.toBeDefined();
+    });
+    test('[5] add', async () => {
+        let result = await Smash.add({ name: 'Falco', series: 'StarFox' })
+        expect(result).toBeDefined()
+        expect(result).toEqual({ id: 4, name: 'Falco', series: 'StarFox' })
+
+        result = await Smash.getAll();
+        expect(result.length).toBe(4);
+    });
+    test('[6] remove', async () => {
+        let result = await Smash.remove(1);
+        expect(result).toHaveProperty('name', 'Kirby');
+        result = await Smash.getAll();
+        expect(result.length).toBe(2);
+    });
+});
+
+describe('', () => {
+    test('[7] GET /smash', async () => {
+        const res = await request(server).get('/smash');
+        expect(res.status).toBe(200);
+        expect(res.body).toHaveLength(3);
+    });
+    // test('[8] GET /smash/:id', async () => {});
+    // test('[9] POST /smash', async () => {});
+    // test('[10] DELETE /smash', async () => {});
+})
