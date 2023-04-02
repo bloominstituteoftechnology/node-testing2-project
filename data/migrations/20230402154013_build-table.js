b8a9f3c6f3cdff1f2eaea1714cddfa37bb7dedb8
Aaron Schuster <aaronjschuster@gmail.com>
2023-04-02 12:07:51 -0400

    add resetdb script and updated migration and seed to include abbreviation field

--- data/migrations/20230402154013_build-table.js
+++ data/migrations/20230402154013_build-table.js
@@ -3,6 +3,7 @@ exports.up = function(knex) {
   return knex.schema.createTable("coasters", table => {
     table.increments("coaster_id");
     table.string("coaster_name").notNullable();
+    table.string("abbrv");
     table.decimal("speed");
     table.decimal("height");
   })
--- data/seeds/sampleCoasters.js
+++ data/seeds/sampleCoasters.js
@@ -1,16 +1,16 @@
 
-exports.seed = function(knex) {
+exports.seed = function (knex) {
   // Deletes ALL existing entries
   return knex('coasters').truncate()
     .then(function () {
       // Inserts seed entries
       return knex('coasters').insert([
-        { coaster_name: "Steel Vengeance", height: 205, speed: 74},
-        { coaster_name: "Millennium Force", height: 310, speed: 93},
-        { coaster_name: "Maverick", height: 105, speed: 70},
-        { coaster_name: "Kingda Ka", height: 456, speed: 128},
-        { coaster_name: "Intimidator 305", height: 305, speed: 90},
-        { coaster_name: "Superman: Escape from Krypton", height: 415, speed: 100}
+        { coaster_name: "Steel Vengeance", abbrv: "SV", height: 205, speed: 74 },
+        { coaster_name: "Millennium Force", abbrv: "MF", height: 310, speed: 93 },
+        { coaster_name: "Maverick", height: 105, speed: 70 },
+        { coaster_name: "Kingda Ka", abbrv: "KK", height: 456, speed: 128 },
+        { coaster_name: "Intimidator 305", abbrv: "I305", height: 305, speed: 90 },
+        { coaster_name: "Superman: Escape from Krypton", abbrv: "SEFK", height: 415, speed: 100 }
       ]);
     });
 };
