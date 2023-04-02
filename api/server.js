5dda5076fc744fcaa5afbd1ed9493e7ad7e51ce0
Aaron Schuster <aaronjschuster@gmail.com>
2023-04-02 12:04:19 -0400

    write out barebones endpoints

--- api/server.js
+++ api/server.js
@@ -4,6 +4,22 @@ const server = express();
 
 server.use(express.json());
 
+server.get("/api/coasters", (req, res) => {
+    res.json({ message: "This would be all the coasters"});
+})
+
+server.get("/api/coasters/:id", (req, res) => {
+    res.json({ message: `This would be all the coaster with id ${req.params.id}` });
+})
+
+server.get("/api/coasters/:abbrv", (req, res) => {
+    res.json({ message: `This would be all the coaster abbreviated as ${req.params.abbrv}` })
+})
+
+server.post("/api/coasters/", (req, res) => {
+    res.status(201).json(req.body);
+})
+
 server.use("*", (req, res) => {
     res.status(404).json({ message: "There's nothing here!"});
 })
