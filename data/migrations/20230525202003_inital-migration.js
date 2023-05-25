4bb0a70ad5d25f118aa789c409a0d25bd73cc6bc
Anthony Climer <anthonyclimer0925@gmail.com>
2023-05-25 17:38:16 -0400

    working on the seed files

--- data/migrations/20230525202003_inital-migration.js
+++ data/migrations/20230525202003_inital-migration.js
@@ -3,7 +3,13 @@
  * @returns { Promise<void> }
  */
 exports.up = async function(knex) {
-  
+  await knex.schema
+    .createTable('recipes', (table) => {
+        table.increments('recipe_id')
+        table.string('recipe_name', 128)
+            .notNullable()
+            .unique()
+    })
 };
 
 /**
@@ -11,5 +17,6 @@ exports.up = async function(knex) {
  * @returns { Promise<void> }
  */
 exports.down = async function(knex) {
-  
+  await knex.schema
+    .dropTableIfExists('recipes')
 };
