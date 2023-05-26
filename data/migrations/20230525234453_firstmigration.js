629fb33ab4b0719a5fac1f015b0280d947ac6f3a
BruvLio <lionel.mbayu@gmail.com>
2023-05-25 19:11:04 -0500

    wip

--- data/migrations/20230525234453_firstmigration.js
+++ data/migrations/20230525234453_firstmigration.js
@@ -2,14 +2,17 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+	return knex.schema.createTable('friends', (tbl) => {
+		tbl.increments();
+		tbl.string('name', 150).unique().notNullable();
+	});
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+	return knex.schema.dropTableIfExists('friends');
 };
--- data/seeds/firstseed.js
+++ data/seeds/firstseed.js
@@ -1,13 +1,14 @@
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
+ * @returns { Promise<void> }
  */
-exports.seed = async function(knex) {
-  // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
-  ]);
+exports.seed = async function (knex) {
+	// Deletes ALL existing entries
+	await knex('friends').del();
+	await knex('friends').insert([
+		{ name: 'Enoka' },
+		{ name: 'Talha' },
+		{ name: 'Amebe' },
+		{ name: 'Jeff' },
+	]);
 };
