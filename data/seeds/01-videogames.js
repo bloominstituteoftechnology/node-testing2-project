220f0e8d5717393a9901f330ed9f7e9ea17d071c
Easton <easton.fritsch21@gmail.com>
2023-10-11 18:53:52 -0600

    added scripts and seed data

--- data/seeds/01-videogames.js
+++ data/seeds/01-videogames.js
@@ -1,13 +1,30 @@
 
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
+exports.seed = async function (knex) {
+  await knex('genere').truncate()
+  await knex('videogames').truncate()
+  await knex('genere').insert([
+    { genere_name: 'BR' },
+    { genere_name: 'MMORPG' },
+    { genere_name: 'Survival' },
+  ])
+  await knex('videogames').insert([
+    {
+      vg_name: 'Minecraft',
+      vg_year: 2009,
+      genere_id: 3,
+      vg_rating: 10
+    },
+    {
+      vg_name: 'PUBG',
+      vg_year: 2017,
+      genere_id: 1,
+      vg_rating: 8
+    },
+    {
+      vg_name: 'World of Warcraft',
+      vg_year: 2004,
+      genere_id: 2,
+      vg_rating: 9
+    },
+  ])
+}
