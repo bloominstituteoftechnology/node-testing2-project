15aa49f0f9f934d547d341686d8533eb2316e17d
lweller86 <112785242+lweller86@users.noreply.github.com>
2023-10-12 11:38:04 -0400

    Set up Tables

--- api/cars/car-router.js
+++ api/cars/car-router.js
@@ -1,23 +1,40 @@
 const router = require("express").Router();
-const Users = require("./users-model.js");
+const Cars = require("./car-model");
 
 
 
-router.get("/", (req, res, next) => { 
-  Users.find()
-    .then(users => {
-      res.json(users);
+
+router.get("/", (req, res, next) => {
+  Cars.find()
+    .then(cars => {
+      res.status(200).json(cars);
     })
     .catch(next);
 });
 
 
 router.get("/:car_id", (req, res, next) => {
-  Users.findById(req.params.user_id)
+  Cars.findById(req.params.user_id)
     .then(user => {
-      res.json(user);
+      res.status(200).json(user);
     })
     .catch(next);
 });
+
+router.post("/", (req, res, next) => {
+  Cars.add(req.body)
+    .then((car) => {
+      res.status(200).json(car);
+    })
+    .catch(next)
+})
+
+router.put("/:car_id", (req, res, next) => {
+  Cars.modify(req.params.car_id, req.body)
+    .then((car) => {
+      res.status(200).json(car);
+    })
+    .catch(next)
+})
 
 module.exports = router;
