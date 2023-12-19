576dc5811a7795e67250558f4904b4a5100a1630
Justin Byrd <justinbyrd7@gmail.com>
2023-12-19 16:27:07 -0600

    adding packages

---
+++ knexfile.js
@@ -0,0 +1,26 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+const common = {
+  client: "sqlite3",
+  useNullAsDefault: true,
+  migrations: { directory: "./data/migrations" },
+  seeds: { directory: "./data/seeds" },
+};
+
+module.exports = {
+  development: {
+    ...common,
+    connection: {
+      filename: "./data/hobbits.db3",
+    },
+  },
+  testing: {
+    ...common,
+    connection: {
+      filename: "./data/test.db3",
+    },
+  },
+};
