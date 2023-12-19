47af19b8932fe2cb1a328e1d12b860ba1a19e1cf
Pamir Perdeci <pamirperdeci@gmail.com>
2023-12-18 19:55:14 -0500

    created a test to be able to update user passwords

--- server.test.js
+++ server.test.js
@@ -35,3 +35,29 @@ describe('DELETE /delete/:username', () => {
 
   // Add more tests as needed, e.g., for non-existing user, invalid username, etc.
 });
+
+
+const request = require('supertest');
+const app = require('./server'); // Adjust the path as necessary
+
+describe('PUT /update-password/:username', () => {
+  it('should update the password for a user', async () => {
+    const username = 'existingUser';
+    const newPassword = 'newPassword123';
+
+    const res = await request(app)
+      .put(`/update-password/${username}`)
+      .send({ password: newPassword });
+    
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('message', 'Password updated successfully');
+
+    // Optionally, you can add more assertions here to verify the password change.
+    // This might involve logging in with the new password, if you have such functionality.
+  });
+
+  // Additional tests can be written for scenarios such as:
+  // - User does not exist
+  // - Invalid password format
+  // - Unauthorized password change attempts
+});
